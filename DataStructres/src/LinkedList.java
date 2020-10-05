b74cdab3295b9916fad7568ba78e60431442c7d3
anjon.roy <anjon.roy@LTPBAN292372211.corp.mphasis.com>
2020-10-06 00:04:30 +0530

    Delete node for a given key

--- DataStructres/src/LinkedList.java
+++ DataStructres/src/LinkedList.java
@@ -30,14 +30,18 @@ public class LinkedList {
 	}
 	
 	//inserting an element in the list at the front
+	
 	public void push(int d) {
+		
 		Node newNode = new Node(d);
 		newNode.next=head;
 		head=newNode;
 	}
 	
 	//inserting an element in the list after a given node
+	
 	public void insertAfter(Node prev_node, int d) {
+		
 		if(prev_node.next==null) {
 			System.out.println("Previous node can not be null");
 		}
@@ -52,6 +56,36 @@ public class LinkedList {
 		prev_node.next=newNode;
 	}
 	
+	//Given a key, delete the first occurrence of the key in linkedlist
+	
+	void deleteNode(int key) {
+		
+		//store head node
+		Node temp=head, prev=null;
+		
+		//if the head node itself holds the key to be deleted
+		if(temp != null && temp.data == key) {
+			
+			head = temp.next; //Changed head
+			return;
+		}
+		
+		//search for the key to be deleted,keep track of the
+		//previous node as we need to change to temp.next
+		
+		while(temp != null && temp.data != key) {
+			prev=temp;
+			temp=temp.next;
+		}
+		
+		//if the key is not present in the linkedlist
+		
+		if(temp==null) return;
+		
+		//unlinking the node from the lists
+		prev.next=temp.next;
+	}
+	
 	public static void main(String[] args) {
 		
 		LinkedList lList =  new LinkedList();
@@ -59,12 +93,24 @@ public class LinkedList {
 		lList.head=new Node(1);
 		Node second=new Node(2);
 		Node third= new Node(3);
+		Node fourth = new Node(6);
+		Node fifth = new Node(7);
+		Node sixth = new Node(8);
 		
 		//Link first node with the second node
 		lList.head.next=second;
 		
 		//Link second node with the third node
 		second.next=third;
+		
+		//Link the third node with the fourth
+		third.next=fourth;
+		
+		//Link the fourth node with the fifth
+		fourth.next=fifth;
+		
+		//Link the fifth node with the sixth
+		fifth.next=sixth;
 		
 		//inserting an element at the front
 		lList.push(4);
@@ -73,6 +119,12 @@ public class LinkedList {
 		lList.insertAfter(second, 5);
 		
 		//Traversing the singly linked List
+		lList.printList();
+		
+		//deleting a key
+		lList.deleteNode(6);
+		
+		System.out.println("Node deleted");
 		lList.printList();
 
 	}
